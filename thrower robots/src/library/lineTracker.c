7738828022bb753704b60e2dc062f9a4d291aeae
YCFELIX\yauch <ycfelix7@gmail.com>
2018-11-03 10:03:33 +0800

    Updated some details for line tracker. Fixed bug.

--- thrower robots/src/library/lineTracker.c
+++ thrower robots/src/library/lineTracker.c
@@ -1,11 +1,13 @@
 #include "lineTracker.h"
 
-# define LineTracker1 &PC3
-# define LineTracker2 &PC4
+//using gpio 8
+# define LineTracker1 &PA7
+//using gpio 7
+# define LineTracker2 &PA6
 
 const GPIOPin* LineTrackers[]={LineTracker1,LineTracker2};
 
-
+ 
 void lineTracker_init(void)
 {
 	gpio_init(LineTracker1, GPIO_Mode_IPU);
@@ -13,7 +15,8 @@ void lineTracker_init(void)
 }
 
 
+//return 1 if read white lines, return 0 if read background
 u8 ReadLineTracker(lineTracker linetracker)
 {
-	return !gpio_read(LineTrackers[linetracker]);
+	return gpio_read(LineTrackers[linetracker]);
 }
