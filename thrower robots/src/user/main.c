12477e55a3b2e802fa59d454b6705ae00402174c
Felix Yau <37495338+ycfelix@users.noreply.github.com>
2018-11-09 18:28:20 +0800

    fully functionally tested

--- thrower robots/src/user/main.c
+++ thrower robots/src/user/main.c
@@ -1,43 +1,315 @@
-/*	
- *	UST Robotics Team Internal 2018
- *
- *  Author  : Anshuman Medhi
- *  Contact : amedhi@connect.ust.hk 
- *						68925193
- *
- */
-
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_exti.h"
+#include "stm32f10x_tim.h"
+#include "stdio.h"
+#include "misc.h"
 #include "main.h"
-
-// Include Library Headers Here
 #include "rcc.h"
 #include "ticks.h"
 #include "gpio.h"
-#include "leds.h"
-#include "buttons.h"
-#include "buzzer.h"
 #include "uart.h"
 #include "lcd_main.h"
-#include "oled.h"
-#include "camera.h"
-#include "pwm.h"
-#include "adc.h"
+#include "leds.h"
 
-int main() {
-	// Initialize Everything Here
-	rcc_init();
-	ticks_init();
-	
-	while (1) {
-		static u32 this_ticks = 0;
-		while (get_ticks() == this_ticks);
-		this_ticks = get_ticks();
+volatile uint8_t FLAG_ECHO = 0;
+volatile uint16_t SonarValue;
 
-		static u32 last_led_ticks=0;
-		if ((this_ticks - last_led_ticks) >= 25) {
-			last_led_ticks = this_ticks;
-			//Code in here will run every 25ms
-			
+void SetSysClockTo72(void)
+{
+	ErrorStatus HSEStartUpStatus;
+    /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
+    /* RCC system reset(for debug purpose) */
+    RCC_DeInit();
+
+    /* Enable HSE */
+    RCC_HSEConfig( RCC_HSE_ON);
+
+    /* Wait till HSE is ready */
+    HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+    if (HSEStartUpStatus == SUCCESS)
+    {
+        /* Enable Prefetch Buffer */
+    	//FLASH_PrefetchBufferCmd( FLASH_PrefetchBuffer_Enable);
+
+        /* Flash 2 wait state */
+        //FLASH_SetLatency( FLASH_Latency_2);
+
+        /* HCLK = SYSCLK */
+        RCC_HCLKConfig( RCC_SYSCLK_Div1);
+
+        /* PCLK2 = HCLK */
+        RCC_PCLK2Config( RCC_HCLK_Div1);
+
+        /* PCLK1 = HCLK/2 */
+        RCC_PCLK1Config( RCC_HCLK_Div2);
+
+        /* PLLCLK = 8MHz * 9 = 72 MHz */
+        RCC_PLLConfig(0x00010000, RCC_PLLMul_9);
+
+        /* Enable PLL */
+        RCC_PLLCmd( ENABLE);
+
+        /* Wait till PLL is ready */
+        while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+        {
+        }
+
+        /* Select PLL as system clock source */
+        RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK);
+
+        /* Wait till PLL is used as system clock source */
+        while (RCC_GetSYSCLKSource() != 0x08)
+        {
+        }
+    }
+    else
+    { /* If HSE fails to start-up, the application will have wrong clock configuration.
+     User can add here some code to deal with this error */
+
+        /* Go to infinite loop */
+        while (1)
+        {
+        }
+    }
+}
+
+void sonar_init() {
+	GPIO_InitTypeDef gpio_cfg;
+	GPIO_StructInit(&gpio_cfg);
+
+	/* Timer TIM3 enable clock */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+	/* Timer TIM3 settings */
+	TIM_TimeBaseInitTypeDef timer_base;
+	TIM_TimeBaseStructInit(&timer_base);
+	timer_base.TIM_CounterMode = TIM_CounterMode_Up;
+	timer_base.TIM_Prescaler = 72;
+	TIM_TimeBaseInit(TIM3, &timer_base);
+	TIM_Cmd(TIM3, ENABLE);
+
+	//Trigger Pin
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	gpio_cfg.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpio_cfg.GPIO_Pin = GPIO_Pin_1;
+	GPIO_Init(GPIOA, &gpio_cfg);
+
+	//EXTI
+
+	/* Set variables used */
+	GPIO_InitTypeDef GPIO_InitStruct;
+	EXTI_InitTypeDef EXTI_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+
+	/* Enable clock for AFIO */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+	/* Set pin as input */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	/* Add IRQ vector to NVIC */
+	/* PB0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI2_IRQn;
+	/* Set priority */
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	/* Set sub priority */
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	/* Enable interrupt */
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	/* Add to NVIC */
+	NVIC_Init(&NVIC_InitStruct);
+
+	/* Tell system that you will use PB0 for EXTI_Line0 */
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource2);
+
+	/* PD0 is connected to EXTI_Line0 */
+	EXTI_InitStruct.EXTI_Line = EXTI_Line2;
+	/* Enable interrupt */
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	/* Interrupt mode */
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	/* Triggers on rising and falling edge */
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	/* Add to EXTI */
+	EXTI_Init(&EXTI_InitStruct);
+}
+
+void EXTI2_IRQHandler(void) {
+	/* Make sure that interrupt flag is set */
+	if (EXTI_GetITStatus(EXTI_Line2) != RESET) {
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_2) != 0) {
+			// Rising
+			TIM_SetCounter(TIM3, 0);
 		}
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_2) == 0) {
+			// Falling
+			SonarValue = TIM_GetCounter(TIM3);
+		}
+
+		/* Clear interrupt flag */
+		EXTI_ClearITPendingBit(EXTI_Line2);
 	}
+}
+
+void sonar_start() {
+	int i;
+
+	GPIO_SetBits(GPIOA, GPIO_Pin_1);
+	//Delay 0x72000
+	for(i=0;i<0x72;i++);
+	GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+}
+
+unsigned int sonar_get() {
+	unsigned long Sonar;
+	// 354000 - Sound speed (mm/sec)
+	// 72000000 - F_CPU
+	// 16 - Timer Prescaler
+	// Result = mm
+	Sonar = (354/2) * (unsigned long)SonarValue / (72000 / 72);
+
+	return (unsigned int)Sonar;
+}
+
+
+void usart_init(void)
+{
+	/* Enable USART1 and GPIOA clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+
+	/* Configure the GPIOs */
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Rx (PA.10) as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART1 */
+	USART_InitTypeDef USART_InitStructure;
+
+	/* USART1 configuration ------------------------------------------------------*/
+	/* USART1 configured as follow:
+	          - BaudRate = 115200 baud
+	          - Word Length = 8 Bits
+	          - One Stop Bit
+	          - No parity
+	          - Hardware flow control disabled (RTS and CTS signals)
+	          - Receive and transmit enabled
+	          - USART Clock disabled
+	          - USART CPOL: Clock is active low
+	          - USART CPHA: Data is captured on the middle
+	          - USART LastBit: The clock pulse of the last data bit is not output to
+	                           the SCLK pin
+	 */
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART1, &USART_InitStructure);
+
+	/* Enable USART1 */
+	USART_Cmd(USART1, ENABLE);
+}
+
+void USARTSend(char *pucBuffer)
+{
+    while (*pucBuffer)
+    {
+        USART_SendData(USART1, *pucBuffer++);
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+        {
+        }
+    }
+}
+
+void TIM4_IRQHandler(void)
+{
+        if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+        {
+        	sonar_start();
+			FLAG_ECHO = 1;
+        	TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+        }
+}
+
+int main(void)
+{
+	char buffer[80] = {'\0'};
+ rcc_init();
+	//gpio_rcc_init(GPIOA);
+    ticks_init();  
+		leds_init();
+	SetSysClockTo72();
+		tft_init(PIN_ON_RIGHT,GREY,RED,WHITE,BLUE);
+		tft_clear();
+    // TIMER4 ?a3÷è ?à ??êóí?ó aèê?èêào 
+    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  	TIM_TimeBaseStructInit(&TIMER_InitStructure);
+    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIMER_InitStructure.TIM_Prescaler = 7200;
+    TIMER_InitStructure.TIM_Period = 5000;
+    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+
+    /* NVIC Configuration */
+    /* Enable the TIM4_IRQn Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+	usart_init();
+	sonar_init();
+	sonar_start();
+	FLAG_ECHO = 1;
+	int ticks=get_ticks();
+    while(1)
+    {
+			
+			while(ticks==get_ticks());
+			ticks=get_ticks();
+
+			if(ticks%100==0)
+			{
+				if (FLAG_ECHO == 1) {
+    		//sprintf(buffer, "%d\r\n", sonar_get());
+    		//USARTSend(buffer);
+					int x=sonar_get();
+					tft_clear();
+							tft_prints(0,0,"sonar position \n%d",x);
+							tft_update();
+					
+					if(x>200){led_on(LED1);led_off(LED2);}
+					else if(x>0&&x<200){led_on(LED2);led_off(LED1);}
+					
+					
+							
+    		FLAG_ECHO = 0;
+    	}
+			}
+    	
+			
+			
+    }
 }
