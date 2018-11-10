1d7acb6a3956f6db040d4d57e0eb1862019e8de2
Felix Yau <37495338+ycfelix@users.noreply.github.com>
2018-11-10 12:32:11 +0800

    update

--- main.c
+++ main.c
@@ -8,56 +8,63 @@
 #include "buttons.h"
 #include "adc.h"
 #include "camera.h"
-#include "lineTracker.h"
+
 
 int led_state=0;
 
-void UARTOnReceiveHandler(const u8 received)
-{
-		if(led_state==0)
-		{
-			led_on(LED1);
-			led_state=1;			
-		}
-		else{led_off(LED1);led_state=0;}
-    return;
-}
+ 
 
 int main() 
 {
     // Initialize Everything Here
     rcc_init();
+	//gpio_rcc_init(GPIOA);
     ticks_init();  
 		leds_init();
 		buttons_init();
 		adc_channel_init(ADC_IO_1);
 		adc_init();
     //call uart_rx_init 
-		lineTracker_init();
+		//lineTracker_init();
 		tft_init(PIN_ON_RIGHT,GREY,RED,WHITE,BLUE);
 		tft_clear();
 	  camera_init(RGBColour);
     //uart_rx_init(COM1,&UARTOnReceiveHandler);
+		//NVIC_Config();
+		//CH_SR04_Init();
 		
 		
     int lastticks=get_ticks();
+		int temp=lastticks;
+		int distance=0;
     while(1)
 		{
-        if(lastticks!=get_ticks())
-				{
+						while( lastticks==get_ticks()){}
             lastticks=get_ticks();
+							
+
+						
+						if(camera_ready())
+{
+	camera_receive_frame();
+	camera_copy();
+	//camera_print(0,0);
+	
+}
+						
+						//tft_prints(0,0,"sonar position %d\n LineTracker state %d",distance,ReadLineTracker(lineTracker1));
+							//tft_update();
 					
-        }
 				
-				if(camera_ready()==1)
-				{
-					camera_receive_frame();
-					camera_copy();
-					camera_print(0,0);
-				}
+					
+    	
+    
+    
+						
+						////tft_prints(0,0,"sonar position %d\n LineTracker state %d",sonar_get(),ReadLineTracker(lineTracker1));
+						//tft_update();
+        		
 				
-				if(ReadLineTracker(lineTracker1)==1){led_on(LED1);}
-				else{led_off(LED1);}
 				
 				
     }        
